0d02aa54d5d0b11afa176c265ec9203ee36080b5
Azimmuss <Azimus076@gmail.com>
2025-10-12 17:28:43 +0600

    lesson_2.py

--- homeworks/homework_2.py
+++ homeworks/homework_2.py
@@ -1,35 +1,38 @@
 class Person:
-    def __init__(self, name, brith_date, occupation):
+    def __init__(self, name, brith_day, occupation):
         self.name = name
-        self.brith_date = brith_date
+        self.brith_day = brith_day
         self.occupation = occupation
 
-
     def introduce(self, data):
-         print(f'Привет, меня зовут {self.name} я друг Азамжана, я родился {self.brith_date}, Работаю {self.occupation}')
+         print(f'Привет, меня зовут {self.name} я друг Азамжана, я родился {self.brith_day}, Работаю {self.occupation}')
+
+
 
 class Friend(Person):
-    def __init__(self, name, brith_date, occupation, hobby):
-        super().__init__(name, brith_date, occupation)
+    def __init__(self, name, brith_day, occupation, hobby):
+        super().__init__(name, brith_day, occupation)
         self.hobby = hobby
 
-    def introduce(self, data):
-         print(f'Привет, меня зовут {self.name} я друг Азамжана, я родился {self.brith_date}, работаю  {self.occupation}, в свободное время {self.hobby}')
+    def introduce(self, hobby):
+         print(f'Привет, меня зовут {self.name} я друг Азамжана, я родился {self.brith_day}, работаю  {self.occupation}, в свободное время {self.hobby}')
 
 
 
 
 
 class Classmate(Person):
-    def __init__(self, name, brith_date, occupation, group_name):
-        super().__init__(name, brith_date, occupation)
+    def __init__(self, name, brith_day, occupation, group_name):
+        super().__init__(name, brith_day, occupation)
         self.group_name = group_name
 
-    def introduce(self, data):
-         print(f'Привет, меня зовут {self.name} я друг Азамжана, я родился в {self.brith_date}, учусь на {self.occupation}, в свободное время {self.group_name}')
+    def introduce(self, group_name):
+         print(f'Привет, меня зовут {self.name} я однаклассник Азамжана, я родился в {self.brith_day}, учусь на {self.occupation},  мы в {self.group_name} группе')
 
 
-classmate = Classmate("Артем", "октябре 23 числа 1990 года",  "", "читаю книги")
-friend = Friend('Кирилл', "11 января 2000 года", "продавцом", hobby="играю в игры")
-friend.introduce()
-classmate.introduce()
+
+
+classmate = Classmate("Артем", "октябре 23 числа 1990 года",  "доктора", 5)
+friend = Friend('Кирилл', "11 января 2000 года", "продавцом", "играю в игры")
+friend.introduce(5)
+classmate.introduce("играю в игры")
